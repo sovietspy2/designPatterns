62ae1d0be1d779248a0001db43ecdb248ed03361
SovietSpy2 <sovietspy2@yandex.com>
2019-01-27 19:20:00 +0100

    refactor

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import factoryPattern from './creationalPatterns/factoryPattern/example';
 
 import prototypePattern from './creationalPatterns/prototypePattern/example';
 import singletonPattern from './creationalPatterns/singletonPattern/example';
-import adapterPattern from './creationalPatterns/adapterPattern/example';
-import compositePattern from './creationalPatterns/compositePattern/example';
-import decoratorPattern from './creationalPatterns/decoratorPattern/example';
+import adapterPattern from './structuralPatterns/adapterPattern/example';
+import compositePattern from './structuralPatterns/compositePattern/example';
+import decoratorPattern from './structuralPatterns/decoratorPattern/example';
 
 // creational pattern examples
 constructorPattern();
